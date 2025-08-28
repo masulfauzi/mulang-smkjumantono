92d5deb4df8d4a14660f966d42644abda2e1517e
Mas'ul <masul.fauzi@gmail.com>
2025-08-28 17:34:56 +0700

    feat: tambah route untuk rekap presensi

--- app/Modules/Presensi/routes.php
+++ app/Modules/Presensi/routes.php
@@ -1,30 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Modules\Presensi\Controllers\PresensiController;
+use Illuminate\Support\Facades\Route;
 
-Route::controller(PresensiController::class)->middleware(['web','auth'])->name('presensi.')->group(function(){
-	Route::get('/presensi', 'index')->name('index');
-	Route::get('/presensi/data', 'data')->name('data.index');
-	Route::get('/presensi/create', 'create')->name('create');
-	Route::post('/presensi', 'store')->name('store');
-	Route::get('/presensi/{presensi}', 'show')->name('show');
-	Route::get('/presensi/{presensi}/edit', 'edit')->name('edit');
-	Route::patch('/presensi/{presensi}', 'update')->name('update');
-	Route::get('/presensi/{presensi}/delete', 'destroy')->name('destroy');
+Route::controller(PresensiController::class)->middleware(['web', 'auth'])->name('presensi.')->group(function () {
+    //custom
+    Route::get('/presensijurnal/{jurnal}', 'presensi_jurnal')->name('jurnal.index');
+    Route::post('/presensijurnal', 'presensi_jurnal_store')->name('jurnal.store');
+    Route::post('/rekappresensi', 'rekap_presensi')->name('rekap.store');
 
+    Route::post('/statussiswa', 'get_siswa_kehadiran')->name('dashboard.index');
 
-
-	//custom
-	Route::get('/presensijurnal/{jurnal}', 'presensi_jurnal')->name('jurnal.index');
-	Route::post('/presensijurnal', 'presensi_jurnal_store')->name('jurnal.store');
-	
-	
-	
-	
-	Route::post('/statussiswa', 'get_siswa_kehadiran')->name('dashboard.index');
-
-
-
+    Route::get('/presensi', 'index')->name('index');
+    Route::get('/presensi/data', 'data')->name('data.index');
+    Route::get('/presensi/create', 'create')->name('create');
+    Route::post('/presensi', 'store')->name('store');
+    Route::get('/presensi/{presensi}', 'show')->name('show');
+    Route::get('/presensi/{presensi}/edit', 'edit')->name('edit');
+    Route::patch('/presensi/{presensi}', 'update')->name('update');
+    Route::get('/presensi/{presensi}/delete', 'destroy')->name('destroy');
 
 });
